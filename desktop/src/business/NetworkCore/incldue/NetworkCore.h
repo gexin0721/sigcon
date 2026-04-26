fe1b1ca8a26f8c415c476ca61f9e55e36d0438bc
xinge <woxobavo456@gmail.com>
2026-04-26 13:19:41 +0800

    编写网络管理类

--- desktop/src/business/NetworkCore/incldue/NetworkCore.h
+++ desktop/src/business/NetworkCore/incldue/NetworkCore.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <ixwebsocket/IXWebSocketServer.h>
+#include <iostream>
 
 
 namespace networkcore
@@ -7,15 +9,34 @@ namespace networkcore
     class NetworkServer
     {
         public:
-        // 
-            NetworkServer(int port);
+            /*
+             *   @brief 初始化网络服务器
+             *   @param port 端口号
+             *   @param onClientMessageCallback 客户端消息回调函数
+             *   @note 
+             */ 
+            NetworkServer(int port,std::function<void(ix::WebSocket&, const ix::WebSocketMessagePtr&)> onClientMessageCallback);
+            
+            /*
+                @brief 析构函数
+                
+            */
             ~NetworkServer();
 
+            /*
+                @brief 启动网络服务器
+            */
             void start();
+
+            /*
+                @brief 停止网络服务器
+            */
             void stop();
 
+            bool send(uint8_t data);
+
         private:
-            
+            ix::WebSocketServer server;
             // 端口号
             int Port = 8000;
             // 运行状态
--- desktop/src/business/NetworkCore/src/NetworkCore.cpp
+++ desktop/src/business/NetworkCore/src/NetworkCore.cpp
@@ -1,5 +1,35 @@
+#include "../include/NetworkCore.h"
 
 namespace networkcore
 {
-    
-}
+    NetworkServer::NetworkServer(int port, std::function<void(ix::WebSocket&, const ix::WebSocketMessagePtr&)> onClientMessageCallback)
+    {
+        // 1. 创建服务器，监听 8080 端口
+        server(port);
+
+        // 2. 设置"有消息/事件来了"的处理函数
+        server.setOnClientMessageCallback(onClientMessageCallback);
+
+        // 3. 开始监听
+        auto [ok, errMsg] = server.listen();
+        if (!ok) {
+            throw std::runtime_error("NetworkServer启动失败: " + errMsg);
+        }
+
+
+    }
+
+    NetworkServer::~NetworkServer()
+    {
+
+    }
+
+    void NetworkServer::start()
+    {      
+        server.start();
+    }
+
+    void NetworkServer::stop()
+    {
+        server.stop();
+    }
